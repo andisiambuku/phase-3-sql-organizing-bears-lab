5a769b3a9a2b7fa98988cb9a6923401c0eda8d4e
andisiambuku <roselandambuku@gmail.com>
2022-10-26 17:25:22 +0300

    Assignment Done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears  VALUES (1,'Mr. Chocolate',5,'M','Black','Calm',0);
+INSERT INTO bears  VALUES (2,'Rowdy',6,'M','Brown','Calm',1);
+INSERT INTO bears  VALUES (3,'Tabitha',7,'F','Black','Calm',0);
+INSERT INTO bears  VALUES (4,'Sergeant Brown',8,'M','Brown','Calm',1);
+INSERT INTO bears  VALUES (5,'Melissa',5,'F','Black','Calm',0);
+INSERT INTO bears  VALUES (6,'Grinch',6,'M','Brown','Calm',1);
+INSERT INTO bears VALUES (7,'Wendy',2,'F','Black','Calm',0);
+INSERT INTO bears  VALUES (8,NULL,9,'M','Brown','Calm',1);
